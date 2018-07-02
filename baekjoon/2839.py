244577b1504197d446d03d97a32d8b611338df88
solo5star <solo_5@naver.com>
2018-07-02 04:50:11 +0000

    Test submimssion commit: id=9068688, title=설탕 배달

--- baekjoon/2839.py
+++ baekjoon/2839.py
@@ -1,12 +1,13 @@
 n = int(input())
 c3 = 0
 c5 = 0
+result = -1;
 for i in range(0, 5):
 	if(n % 5 == 0):
 		c5 = n // 5
-		print(c3 + c5)
-		return
+		result = c3 + c5
+		break
 	n -= 3
 	c3 += 1
 
-print(-1)
+print(result)
