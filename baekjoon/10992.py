b9f25868f5b22c9d2c7194af22bfc9f709921775
solo5star <solo_5@naver.com>
2020-10-14 17:17:34 +0000

    Test submimssion commit: id=23250047, title=별 찍기 - 17

---
+++ baekjoon/10992.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+	if i == 0:
+		print(" " * (n - 1) + "*")
+	elif i == n - 1:
+		print("*" * (2 * n - 1))
+	else:
+		print(" " * (n - i - 1) + "*" + " " * (i * 2 - 1) + "*")
