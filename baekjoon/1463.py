5b694a7cb654f8b5a1b6667d11a7d2c44e1de874
solo5star <solo_5@naver.com>
2020-10-14 18:37:25 +0000

    Test submimssion commit: id=23250620, title=1로 만들기

--- baekjoon/1463.py
+++ baekjoon/1463.py
@@ -2,17 +2,23 @@ import sys
 
 n = int(sys.stdin.readline())
 
-count = 0
-while n != 1:
-	if (n - 1) % 3 == 0:
-		n -= 1
-	elif n % 3 == 0:
-		n //= 3
-	elif n % 2 == 0:
-		n //= 2
-	else:
-		n -= 1
-	
-	count += 1
+# 숫자 0 = None
+# 숫자 1 = 0
+dp = [None, 0]
 
-print(count)
+# Bottom Up 구현
+for i in range(2, n + 1):
+	# Case 1 - 이전 숫자 + 1
+	dp.append(dp[i - 1] + 1)
+
+	# Case 2 - 2로 나눌 수 있는지
+	# 1을 빼는 것이 유리한지
+	# 2로 나누는 것이 유리한지 비교
+	if i % 2 == 0:
+		dp[i] = min(dp[i], dp[i // 2] + 1)
+	
+	# Case 3 - 3으로 나눌 수 있는지
+	if i % 3 == 0:
+		dp[i] = min(dp[i], dp[i // 3] + 1)
+
+print(dp[n])
