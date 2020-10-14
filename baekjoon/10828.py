d77a279c5fbf37126b04115286501c8d7ebed7fd
solo5star <solo_5@naver.com>
2020-10-14 18:12:12 +0000

    Test submimssion commit: id=23250490, title=스택

--- baekjoon/10828.py
+++ baekjoon/10828.py
@@ -10,10 +10,10 @@ for i in range(n):
 	if inst[0] == "push":
 		stack.append(inst[1])
 	elif inst[0] == "top":
-		print(stack[-1] if len(stack) == 0 else -1)
+		print(stack[-1] if len(stack) > 0 else -1)
 	elif inst[0] == "size":
 		print(len(stack))
 	elif inst[0] == "empty":
 		print(1 if len(stack) == 0 else 0)
 	elif inst[0] == "pop":
-		print(stack.pop() if len(stack) == 0 else -1)
+		print(stack.pop() if len(stack) > 0 else -1)
