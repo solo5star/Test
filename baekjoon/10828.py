c8d8adb14b17a510ed5b16f8c241a91ccd75feca
solo5star <solo_5@naver.com>
2020-10-14 18:08:00 +0000

    Test submimssion commit: id=23250468, title=스택

---
+++ baekjoon/10828.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+
+for i in range(n):
+	inst = sys.stdin.readline().strip().split()
+	
+	if inst[0] == "push":
+		stack.append(int(inst[1]))
+	elif inst[0] == "top":
+		print(stack[-1] if len(stack) == 0 else -1)
+	elif inst[0] == "size":
+		print(len(stack))
+	elif inst[0] == "empty":
+		print(1 if len(stack) == 0 else 0)
+	elif inst[0] == "pop":
+		print(stack.pop() if len(stack) == 0 else -1)
