714a40693482a3628a51fa9b9b0c516104f04f6c
solo5star <solo_5@naver.com>
2020-10-17 14:48:46 +0000

    Test submimssion commit: id=23314318, title=수 찾기

--- baekjoon/1920.cpp
+++ baekjoon/1920.cpp
@@ -5,25 +5,52 @@
 
 using namespace std;
 
-bool nums[100001];
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int* nums;
+
 	int n, m, a;
 	cin >> n;
 
+	nums = new int[n];
+
 	for (int i = 0; i < n; i++) {
 		cin >> a;
-		nums[a] = true;
+		nums[i] = a;
 	}
+
+	sort(nums, nums+n);
 
 	cin >> m;
 
 	for (int i = 0; i < m; i++) {
-		cin >> a;
-		cout << (nums[a] ? 1 : 0) << "\n";
+		int search;
+		cin >> search;
+
+		int low = 0;
+		int high = n;
+		bool found = false;
+
+		while (low != high) {
+			int mid = (low + high) / 2;
+
+			if (nums[mid] == search) {
+				found = true;
+				break;
+			}
+			else if (search < nums[mid]) {
+				high = mid - 1;
+			}
+			else if (nums[mid] < search) {
+				low = mid + 1;
+			}
+		}
+
+		cout << (found ? 1 : 0) << "\n";
 	}
+
+	delete[] nums;
 }
