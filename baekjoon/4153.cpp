979481cbeda2d6a0beae277ba71a3264d2a72c23
solo5star <solo_5@naver.com>
2020-10-20 04:01:20 +0000

    Test submimssion commit: id=23356912, title=직각삼각형

---
+++ baekjoon/4153.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n1, n2, n3;
+
+	while (true) {
+		cin >> n1 >> n2 >> n3;
+
+		if ((n1 | n2 | n3) == 0) break;
+
+		if (n1 * n1 + n2 * n2 == n3 * n3) {
+			cout << "right\n";
+		}
+		else {
+			cout << "wrong\n";
+		}
+	}
+}
