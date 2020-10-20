fdbb2d0a5c3d5a4f07bd48fed5cb26a7f8a13042
solo5star <solo_5@naver.com>
2020-10-20 04:16:16 +0000

    Test submimssion commit: id=23357106, title=Hashing

---
+++ baekjoon/15829.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	string s;
+
+	cin >> n;
+	cin >> s;
+
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += (s[i] - 'a' + 1) * (int)pow(31, i);
+	}
+	sum %= 1234567891;
+
+	cout << sum;
+}
