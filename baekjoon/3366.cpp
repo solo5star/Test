8ee869b544b393d89c1d8be1f21f630982d9977a
solo5star <solo_5@naver.com>
2020-10-30 15:07:31 +0000

    Test submimssion commit: id=23562999, title=수열 줄이기

--- baekjoon/3366.cpp
+++ baekjoon/3366.cpp
@@ -6,26 +6,27 @@
 #include <math.h>
 #include <unordered_map>
 #include <string>
+#include <math.h>
 
 using namespace std;
-
-int numbers[1000000];
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n, number, m, acc = 0;
+	int n, number, prev, acc = 0;
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		cin >> number;
-		if (i == 0) m = number;
-		else m = m > number ? number : m;
 
-		acc += number;
+		if (i != 0) {
+			acc += max(prev, number);
+		}
+
+		prev = number;
 	}
 
-	cout << (acc - m);
+	cout << acc;
 }
