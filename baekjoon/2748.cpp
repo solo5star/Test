d217ef323149479d49f19c934cbfc1c80bf1a03e
solo5star <solo_5@naver.com>
2020-11-04 03:51:31 +0000

    Test submimssion commit: id=23642213, title=피보나치 수 2

---
+++ baekjoon/2748.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int dp[91] = { 0, 1 };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	cout << dp[n];
+}
