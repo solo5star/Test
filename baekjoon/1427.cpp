e33fa729a39bd7929b41b6df1643a24ec3908e7b
solo5star <solo_5@naver.com>
2020-11-06 13:55:27 +0000

    Test submimssion commit: id=23698260, title=소트인사이드

---
+++ baekjoon/1427.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <queue>
+
+using namespace std;
+
+int nums[10];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string input;
+	cin >> input;
+	
+	for (int i = 0; i < input.length(); i++) {
+		nums[input[i] - '0']++;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < nums[i]; j++) {
+			cout << i;
+		}
+	}
+}
