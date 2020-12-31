1bd9e6c7ad5911260d378cc68491b90f5974a14a
solo5star <solo_5@naver.com>
2020-12-31 16:53:42 +0000

    Test submimssion commit: id=24791318, title=Fly me to the Alpha Centauri

---
+++ baekjoon/1011.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int sol(int diff) {
+	int acc = 0;
+	while (diff > 3) {
+		acc += (diff % 2) + 2;
+		diff = (diff - 2) / 2;
+	}
+	acc += diff;
+	return acc;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t, x, y, diff;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> x >> y;
+
+		diff = y - x;
+		cout << sol(diff) << "\n";
+	}
+}
