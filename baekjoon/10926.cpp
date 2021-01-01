9f4d635998bbafa15c1f5f88da7411814bbdb3da
solo5star <solo_5@naver.com>
2021-01-01 12:11:13 +0000

    Test submimssion commit: id=24809601, title=??!

---
+++ baekjoon/10926.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string input;
+	cin >> input;
+	cout << input << "??!";
+}
