3c05a81b6aa1e2de2541d3123e03b5f224db637c
solo5star <solo_5@naver.com>
2021-01-01 12:01:27 +0000

    Test submimssion commit: id=24809293, title=세로읽기

---
+++ baekjoon/10798.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+char str[5][16];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 0; i < 5; i++) {
+		cin >> str[i];
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << str[j][i];
+		}
+	}
+}
