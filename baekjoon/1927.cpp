516c7bf4d0057539e1d4f3e8fbdce55188a4c627
solo5star <solo_5@naver.com>
2021-01-01 07:09:40 +0000

    Test submimssion commit: id=24801147, title=최소 힙

---
+++ baekjoon/1927.cpp
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+#include <unordered_set>
+
+using namespace std;
+
+int heap[100000];
+int index = 0;
+
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void push_heap(int n) {
+	heap[index] = n;
+
+	int current = index;
+	while (current > 0) {
+		int parent = (index+1) / 2 - 1;
+
+		if (heap[parent] > heap[current]) {
+			swap(&heap[parent], &heap[current]);
+			current = parent;
+		}
+		else {
+			break;
+		}
+	}
+
+	index++;
+}
+
+int pop_heap() {
+	int popped = heap[0];
+
+	heap[0] = heap[index - 1];
+	index--;
+
+	int current = 0;
+	while (true) {
+		int child = (current * 2) + 1;
+		if (!(child < index)) break;
+
+		int child2 = (current * 2) + 2;
+		if (child2 < index && heap[child] > heap[child2]) child = child2;
+
+		if (heap[current] < heap[child]) break;
+
+		swap(&heap[current], &heap[child]);
+		current = child;
+	}
+
+	return popped;
+}
+
+void print_heap() {
+	cout << "[ ";
+	for (int i = 0; i < index; i++) {
+		cout << heap[i] << " ";
+	}
+	cout << "]\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, ins;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> ins;
+
+		if (ins == 0) {
+			if (index == 0) {
+				cout << 0;
+			}
+			else {
+				cout << pop_heap();
+			}
+			cout << "\n";
+		}
+		else {
+			push_heap(ins);
+		}
+	}
+}
