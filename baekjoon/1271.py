8786c9716edda4fc833ab47a0fddc3efc4884bb1
solo5star <solo_5@naver.com>
2021-01-02 16:05:52 +0000

    Test submimssion commit: id=24849355, title=엄청난 부자2

--- baekjoon/1271.py
+++ baekjoon/1271.py
@@ -1,9 +1,6 @@
-import sys
+n, m = map(int, input().split())
 
-n, m = map(int, sys.stdin.readline().split(" "))
+assert n >= m
 
-a = (n // (10 * m)) * 10
-b = n % (a * m) if a > 0 else n
-
-print(a)
-print(b)
+print(n // m)
+print(n % m)
