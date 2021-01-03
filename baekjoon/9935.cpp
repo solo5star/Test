b28646137559ce475373e12bbc0b283d978af7ad
solo5star <solo_5@naver.com>
2021-01-03 10:57:34 +0000

    Test submimssion commit: id=24870714, title=문자열 폭발

--- baekjoon/9935.cpp
+++ baekjoon/9935.cpp
@@ -8,36 +8,43 @@
 using namespace std;
 
 char str[1000001];
-bool explodes[256];
+char delim[37];
+
+int rIndex = 0;
+int mIndex = 0;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	string explode;
-	cin >> str >> explode;
+	cin >> str;
+	cin >> delim;
 
-	int len = explode.length();
-	for (int i = 0; i < len; i++) {
-		explodes[explode[i]] = true;
-	}
+	int len_str = strlen(str);
+	int len_delim = strlen(delim);
+	int matches;
 
-	int modifyIdx = 0;
-	int searchIdx = 0;
-	while (str[searchIdx] != '\0') {
-		if (explodes[str[searchIdx]]) {
-			searchIdx++;
-			continue;
+	while(mIndex < len_str) {
+		str[rIndex] = str[mIndex];
+
+		matches = 0;
+		for (int i = 0; i < len_delim; i++) {
+			if (rIndex - i < 0) break;
+
+			if (str[rIndex - i] != delim[len_delim - i - 1]) break;
+			
+			matches++;
 		}
 
-		// skip explode character
-		str[modifyIdx] = str[searchIdx];
+		if (matches == len_delim) {
+			rIndex -= len_delim;
+		}
 
-		modifyIdx++;
-		searchIdx++;
+		rIndex++;
+		mIndex++;
 	}
-	str[modifyIdx] = '\0';
+	str[rIndex] = '\0';
 
-	cout << (modifyIdx ? str : "FRULA");
+	cout << (str[0] ? str : "FRULA");
 }
