8ae631777452eff19dffe6b62ed8078bc712561c
solo5star <solo_5@naver.com>
2021-01-03 19:50:11 +0000

    Test submimssion commit: id=24884821, title=가장 긴 감소하는 부분 수열

---
+++ baekjoon/11722.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+
+using namespace std;
+
+int numbers[1000];
+int dp[1000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, maximum = 0;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> numbers[i];
+
+		for (int j = 0; j < i; j++) {
+			if (numbers[j] > numbers[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+				maximum = max(maximum, dp[i]);
+			}
+		}
+	}
+
+	cout << maximum + 1;
+}
