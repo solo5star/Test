cfe28ee767b24f56c25280b4768e4d5c9fe85a7c
solo5star <solo_5@naver.com>
2021-01-04 11:12:43 +0000

    Test submimssion commit: id=24907120, title=A → B

---
+++ baekjoon/16953.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+
+using namespace std;
+
+typedef unsigned long long uint_64;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	uint_64 limit = 1000000000;
+	uint_64 a, b;
+	int count = 0;
+	cin >> a >> b;
+
+	while (a < b) {
+		if (b % 10 == 1) {
+			b /= 10;
+		}
+		else if (b % 2 == 0) {
+			b /= 2;
+		}
+		count++;
+	}
+
+	count = a == b ? count + 1 : -1;
+
+	cout << count;
+}
