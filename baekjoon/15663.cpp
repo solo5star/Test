39014e82829884bb2f16be9e0d8fe57c10bdb936
solo5star <solo_5@naver.com>
2021-01-08 11:47:10 +0000

    Test submimssion commit: id=25060454, title=N과 M (9)

---
+++ baekjoon/15663.cpp
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <unordered_set>
+
+using namespace std;
+
+int numbers[9];
+bool visited[9];
+int stack[9];
+int n, m;
+
+unordered_set<string> sequences;
+
+string makeSequence() {
+	string sequence = "";
+	for (int i = 1; i <= m; i++) {
+		sequence += to_string(stack[i]) + (i == m ? "\n" : " ");
+	}
+	return sequence;
+}
+
+void dfs(int index, int depth = 1) {
+	if (visited[index]) return;
+
+	stack[depth] = numbers[index];
+
+	if (depth == m) {
+		string sequence = makeSequence();
+
+		if (!sequences.count(sequence)) {
+			cout << sequence;
+		}
+		sequences.insert(sequence);
+	}
+	else if (depth < m) {
+		visited[index] = true;
+		for (int i = 0; i < n; i++) {
+			dfs(i, depth + 1);
+		}
+		visited[index] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) cin >> numbers[i];
+
+	sort(numbers, numbers + n);
+
+	for (int i = 0; i < n; i++) dfs(i);
+}
