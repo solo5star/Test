cf730c8f0ffe7f52073341a5b2612009f6abbde3
solo5star <solo_5@naver.com>
2021-01-09 12:20:13 +0000

    Test submimssion commit: id=25094986, title=맥주 마시면서 걸어가기

---
+++ baekjoon/9205.cpp
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <queue>
+
+using namespace std;
+
+typedef struct { int x; int y; } point;
+
+int nodesCount = 0;
+
+const int NODE_START = 1;
+const int NODE_END = 2;
+
+// 1 = START
+// 2 = END
+// 3~n = CONVINIENT
+point nodes[102];
+int distances[102][102];
+bool visited[102];
+
+queue<int> q;
+
+int distance(point& from, point& to) {
+	return abs(from.x - to.x) + abs(from.y - to.y);
+}
+
+bool bfs() {
+	bool success = false;
+
+	while (!q.empty()) {
+		int nodeId = q.front();
+		q.pop();
+
+		visited[nodeId] = true;
+
+		for (int i = 1; i <= nodesCount; i++) {
+			if (visited[i]) continue;
+
+			if (distances[nodeId][i] <= 1000) {
+				if (i == NODE_END) {
+					while (!q.empty()) q.pop();
+					success = true;
+					break;
+				}
+				else {
+					q.push(i);
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t, n, x, y;
+	cin >> t;
+
+	while (t--) {
+		cin >> n;
+
+		nodesCount = n + 2;
+
+		cin >> x >> y;
+		nodes[NODE_START] = { x, y };
+		for (int i = 3; i <= nodesCount; i++) {
+			cin >> x >> y;
+			nodes[i] = { x, y };
+		}
+		cin >> x >> y;
+		nodes[NODE_END] = { x, y };
+
+		for (int via = 1; via <= nodesCount; via++) {
+			for (int from = 1; from <= nodesCount; from++) {
+				for (int to = 1; to <= nodesCount; to++) {
+					if (distances[from][to] == 0) distances[from][to] = distance(nodes[from], nodes[to]);
+
+					distances[from][to] = min(
+						distances[from][to],
+						distance(nodes[from], nodes[via]) + distance(nodes[via], nodes[to])
+					);
+				}
+			}
+		}
+
+		q.push(NODE_START);
+		cout << (bfs() ? "happy" : "sad") << "\n";
+	}
+}
