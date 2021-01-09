c5c1619211570e73858ad8142cd534009285fe10
solo5star <solo_5@naver.com>
2021-01-09 11:21:21 +0000

    Test submimssion commit: id=25093061, title=Hashing

--- baekjoon/15829.py
+++ baekjoon/15829.py
@@ -1,16 +1,12 @@
 import sys
 
 n = int(sys.stdin.readline())
-s = sys.stdin.readline().strip()
 
-dp = [1, 31]
-for i in range(2, n+1):
-    dp.append(dp[i - 1] * 31);
+text = sys.stdin.readline()
 
-result = 0
+acc = 0
 for i in range(n):
-    result += (ord(s[i]) - ord('a') + 1) * dp[i]
-    
-result %= 1234567891
+    acc += (ord(text[i]) - ord('a') + 1) * (31 ** i) % 1234567891
+    acc %= 1234567891
 
-print(result)
+print(acc)
