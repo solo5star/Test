93af7728843cb1cf1bce1b43b81951fa0e5b1017
solo5star <solo_5@naver.com>
2021-01-10 17:07:06 +0000

    Test submimssion commit: id=25136216, title=트리의 높이와 너비

---
+++ baekjoon/2250.cpp
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <cstring>
+
+using namespace std;
+
+typedef struct {
+	int id;
+	int left;
+	int right;
+	int x, y;
+} node;
+
+node nodes[10001];
+
+int maxLevel = 1;
+struct { int min; int max; int width; } levels[10001];
+
+int x_acc = 1;
+
+void walk(int id = 1, int level = 1) {
+	if (nodes[id].left) walk(nodes[id].left, level + 1);
+
+	int x = x_acc++;
+
+	nodes[id].x = x;
+	nodes[id].y = level;
+
+	// UPDATE LEVEL's MIN/MAX
+	if (x < levels[level].min) levels[level].min = x;
+	if (levels[level].max < x) levels[level].max = x;
+
+	// UPDATE LEVEL's WIDTH
+	if (levels[level].min && levels[level].max) {
+		levels[level].width = levels[level].max - levels[level].min;
+	}
+	else {
+		levels[level].width = 1;
+	}
+
+	// UPDATE MAXIMUM LEVEL
+	if (levels[maxLevel].width == levels[level].width && level < maxLevel) {
+		maxLevel = level;
+	}
+	if (levels[maxLevel].width < levels[level].width) {
+		maxLevel = level;
+	}
+
+	if (nodes[id].right) walk(nodes[id].right, level + 1);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, id, left, right;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> id >> left >> right;
+
+		nodes[id].left = left == -1 ? 0 : left;
+		nodes[id].right = right == -1 ? 0 : right;
+	}
+
+	walk();
+
+	cout << maxLevel << " " << levels[maxLevel].width;
+}
