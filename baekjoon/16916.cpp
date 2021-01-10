6a2cef0ea496f0b7205c0d632665fd4a18b06a42
solo5star <solo_5@naver.com>
2021-01-10 13:49:57 +0000

    Test submimssion commit: id=25130404, title=부분 문자열

---
+++ baekjoon/16916.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+string text;
+string pattern;
+
+int pi[1000000];
+
+void calculatePi() {
+	int len = pattern.length();
+	int j = 0;
+	for (int i = 1; i < len; i++) {
+		while (j > 0 && pattern[j] != pattern[i]) {
+			j = pi[j - 1];
+		}
+		if (pattern[j] == pattern[i]) {
+			pi[i] = ++j;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	getline(cin, text);
+	getline(cin, pattern);
+
+	calculatePi();
+
+	int len = text.length();
+	int j = 0;
+
+	bool found = false;
+
+	for (int i = 0; i < len; i++) {
+		while (j > 0 && text[i] != pattern[j]) {
+			j = pi[j - 1];
+		}
+
+		if (text[i] == pattern[j]) {
+			if (j == pattern.length() - 1) {
+				found = true;
+				break;
+			}
+			else {
+				j++;
+			}
+		}
+	}
+
+	cout << (found ? 1 : 0);
+}
