69346a53bced2af71de7b8fd2109cfb875512cc7
solo5star <solo_5@naver.com>
2021-01-10 15:14:30 +0000

    Test submimssion commit: id=25133860, title=문자열 제곱

---
+++ baekjoon/4354.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int pi[1000000];
+string text;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true) {
+		cin >> text;
+		if (text == ".") break;
+
+		int len = text.length();
+
+		int j = 0;
+		for (int i = 1; i < len; i++) {
+			while (j > 0 && text[j] != text[i]) {
+				j = pi[j - 1];
+			}
+			if (text[j] == text[i]) {
+				pi[i] = ++j;
+			}
+		}
+
+		int patternLength = len - pi[len - 1];
+
+		cout << (len % patternLength ? len : len / patternLength) << "\n";
+	}
+}
