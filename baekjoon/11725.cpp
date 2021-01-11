8a69db80e154f96aa7419788b90f9587e0ab81d9
solo5star <solo_5@naver.com>
2021-01-11 15:34:17 +0000

    Test submimssion commit: id=25173810, title=트리의 부모 찾기

--- baekjoon/11725.cpp
+++ baekjoon/11725.cpp
@@ -7,10 +7,14 @@
 #include <vector>
 #include <math.h>
 #include <cstring>
+#include <queue>
 
 using namespace std;
 
+vector<int> nodes[100001];
 int parents[100001];
+
+queue<int> q;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -20,18 +24,28 @@ int main() {
 	int n, a, b;
 	cin >> n;
 
-	parents[1] = 1;
 	for (int i = 1; i < n; i++) {
 		cin >> a >> b;
 
-		// parent of node a is exists
-		if (parents[a]) parents[b] = a;
-
-		// parent of node b is exists (may)
-		else parents[a] = b;
+		nodes[a].push_back(b);
+		nodes[b].push_back(a);
 	}
 
-	for(int i = 2; i <= n; i++) {
+	q.push(1);
+	while (!q.empty()) {
+		int parent = q.front();
+		q.pop();
+
+		for (int child : nodes[parent]) {
+			if (parents[child]) continue;
+
+			parents[child] = parent;
+
+			q.push(child);
+		}
+	}
+
+	for (int i = 2; i <= n; i++) {
 		cout << parents[i] << "\n";
 	}
 }
