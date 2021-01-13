19418169ea4bb15ec670aaf338b2529e6c7094dc
solo5star <solo_5@naver.com>
2021-01-13 16:07:16 +0000

    Test submimssion commit: id=25251708, title=ACM Craft

---
+++ baekjoon/1005.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <queue>
+
+using namespace std;
+
+typedef struct {
+	int cost;
+	int requiredCost;
+	int inDegree;
+	vector<int> children;
+} node;
+
+node nodes[1001];
+
+int nodesCount;
+int edgesCount;
+
+queue<int> q;
+
+void calculateCosts() {
+	for (int i = 1; i <= nodesCount; i++) {
+		if (nodes[i].inDegree == 0) {
+			q.push(i);
+		}
+	}
+
+	while (!q.empty()) {
+		node& n = nodes[q.front()];
+		q.pop();
+
+		n.cost += n.requiredCost;
+
+		for (int id : n.children) {
+			node& children = nodes[id];
+			children.requiredCost = max(n.cost, children.requiredCost);
+			children.inDegree--;
+
+			if (children.inDegree == 0) q.push(id);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int t, cost, from, to, destination;
+	cin >> t;
+	while (t--) {
+		cin >> nodesCount >> edgesCount;
+
+		for (int i = 1; i <= nodesCount; i++) {
+			cin >> nodes[i].cost;
+		}
+
+		for (int i = 0; i < edgesCount; i++) {
+			cin >> from >> to;
+
+			nodes[from].children.push_back(to);
+			nodes[to].inDegree++;
+		}
+
+		cin >> destination;
+
+		calculateCosts();
+
+		cout << nodes[destination].cost << "\n";
+	}
+}
