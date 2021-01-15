1b07ee3d364fc6f417297d537c239d172e09ec39
solo5star <solo_5@naver.com>
2021-01-15 07:52:33 +0000

    Test submimssion commit: id=25305433, title=HTML

---
+++ baekjoon/6581.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <queue>
+#include <limits.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	string word;
+	int wordCounter = 0;
+
+	while (!cin.eof()) {
+		cin >> word;
+
+		if (word == "<br>") {
+			cout << "\n";
+			wordCounter = 0;
+		}
+		else if (word == "<hr>") {
+			if(wordCounter) cout << "\n";
+			for (int i = 0; i < 80; i++) cout << "-";
+			cout << "\n";
+			wordCounter = 0;
+		}
+		else {
+			if (wordCounter + word.length() + 1 > 80) {
+				cout << "\n";
+				wordCounter = 0;
+			}
+			else {
+				cout << (wordCounter ? " " : "");
+			}
+
+			cout << word;
+			wordCounter += word.length() + 1;
+		}
+	}
+}
