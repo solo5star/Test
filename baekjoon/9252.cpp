fb97ff5e4199784fc0c14c37554496d71da6d643
solo5star <solo_5@naver.com>
2021-01-16 17:34:07 +0000

    Test submimssion commit: id=25352849, title=LCS 2

--- baekjoon/9252.cpp
+++ baekjoon/9252.cpp
@@ -19,19 +19,24 @@ int main() {
 	cout.tie(nullptr);
 
 	string str1, str2;
-	cin >> str2 >> str1;
+	cin >> str1 >> str2;
 
 	int maxValue = 0;
 
 	for (int i = 0; i < str1.length(); i++) {
 		for (int j = 0; j < str2.length(); j++) {
-			dp[i + 1][j + 1] = max(max(
-				// if character matches, previous +1
-				dp[i][j] + (str1[i] == str2[j]),
-				// previous (left)
-				dp[i + 1][j]),
-				// previous (above)
-				dp[i][j + 1]);
+			// if character matches, previous +1
+			if (str1[i] == str2[j]) {
+				dp[i + 1][j + 1] = dp[i][j] + 1;
+			}
+			else {
+				dp[i + 1][j + 1] = max(
+					// previous (above)
+					dp[i][j + 1],
+					// previous (left)
+					dp[i + 1][j]
+				);
+			}
 
 			maxValue = max(maxValue, dp[i + 1][j + 1]);
 		}
@@ -42,13 +47,17 @@ int main() {
 	stack<char> ss;
 	if (maxValue) {
 		cout << "\n";
-		int i = str1.length();
 
-		for (int j = str2.length(); j > 0; j--) {
-			if (dp[i][j] != dp[i][j - 1]) {
-				ss.push(str2[j - 1]);
-				i--;
-			}
+		int i = str1.length(), j = str2.length();
+
+		while (i > 0 && j > 0) {
+			while (dp[i][j] == dp[i][j - 1]) j--;
+			while (dp[i][j] == dp[i - 1][j]) i--;
+
+			ss.push(str1[i - 1]);
+
+			i--;
+			j--;
 		}
 
 		while (!ss.empty()) {
