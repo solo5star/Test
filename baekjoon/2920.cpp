5c2221c93b83266184309867db35cb1a9181cb31
solo5star <solo_5@naver.com>
2021-01-17 16:39:08 +0000

    Test submimssion commit: id=25381477, title=음계

--- baekjoon/2920.cpp
+++ baekjoon/2920.cpp
@@ -1,30 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <utility>
 #include <algorithm>
+#include <string>
+#include <vector>
 #include <math.h>
+#include <queue>
+#include <stack>
+#include <limits.h>
 
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-	int next;
-	int prev;
+	bool ascending = false, descending = false;
 
-	bool asc = false;
-	bool dsc = false;
-
+	int num[8];
 	for (int i = 0; i < 8; i++) {
-		if (i == 0) cin >> prev;
-		else {
-			cin >> next;
-			asc = asc || next > prev;
-			dsc = dsc || next < prev;
-			prev = next;
+		cin >> num[i];
+
+		if (i > 0) {
+			ascending |= num[i] > num[i - 1];
+			descending |= num[i] < num[i - 1];
 		}
 	}
 
-	cout << (asc && dsc ? "mixed" : asc ? "ascending" : dsc ? "descending" : "");
+	cout << (ascending && descending ? "mixed" : ascending ? "ascending" : "descending");
 }
