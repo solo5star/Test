677b12830895e47dd344ec01a3a95a0dd1b48add
solo5star <solo_5@naver.com>
2021-01-17 16:01:22 +0000

    Test submimssion commit: id=25380856, title=Java vs C++

--- baekjoon/3613.js
+++ baekjoon/3613.js
@@ -6,11 +6,11 @@ const rl = readline.createInterface({
 
 rl.on('line', line => {
 	switch(true) {
-		case /[a-z]([A-Z][a-z]*)+/.test(line):
+		case /[a-z]([A-Z][a-z]*)*/.test(line):
 			console.log(line.replace(/[A-Z]/g, s => '_' + s.toLowerCase()));
 			break;
 
-		case /[a-z]+(_[a-z]+)+/.test(line):
+		case /[a-z]+(_[a-z]+)*/.test(line):
 			console.log(line.replace(/_[a-z]/g, s => s.substring(1, 2).toUpperCase()));
 			break;
 
