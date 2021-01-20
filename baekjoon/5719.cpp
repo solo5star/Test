39a50245ed2efe066c38e0f338bcfae73663fb85
solo5star <solo_5@naver.com>
2021-01-20 10:57:30 +0000

    Test submimssion commit: id=25483742, title=거의 최단 경로

--- baekjoon/5719.cpp
+++ baekjoon/5719.cpp
@@ -17,26 +17,11 @@ bool operator<(const path& a, const path& b) {
 	return a.cost > b.cost;
 }
 
-vector<vector<edge>> nodes(500);
-int nodeCount;
-int edgeCount;
-
 int trueVal = 1;
 int banned[500][500];
 
-vector<vector<int>> trace(500);
-vector<int> costs(500);
-
-bool isBetterPath(path& p) {
-	if (banned[p.mid][p.to] == trueVal) return false;
-
-	if (costs[p.to] < p.cost) return false;
-
-	return true;
-}
-
-int calculateShortestPath(int from, int to) {
-	fill(costs.begin(), costs.end(), INT_MAX);
+int calculateShortestPath(vector<vector<edge>>& nodes, vector<vector<int>>& traces, int from, int to) {
+	vector<int> costs(500, INT_MAX);
 
 	costs[from] = 0;
 
@@ -50,14 +35,16 @@ int calculateShortestPath(int from, int to) {
 		path p = pq.top();
 		pq.pop();
 
-		if (!isBetterPath(p)) continue;
+		if (banned[p.mid][p.to] == trueVal) continue;
+
+		if (costs[p.to] < p.cost) continue;
 
 		if (costs[p.to] == p.cost) {
-			trace[p.to].push_back(p.mid);
+			traces[p.to].push_back(p.mid);
 		}
 		else {
-			trace[p.to].clear();
-			trace[p.to].push_back(p.mid);
+			traces[p.to].clear();
+			traces[p.to].push_back(p.mid);
 		}
 
 		costs[p.to] = p.cost;
@@ -65,7 +52,7 @@ int calculateShortestPath(int from, int to) {
 		for (edge& e : nodes[p.to]) {
 			path _p = { p.to, e.to, p.cost + e.cost };
 
-			if (!isBetterPath(_p)) continue;
+			if (banned[_p.mid][_p.to] == trueVal) continue;
 
 			pq.push(_p);
 		}
@@ -74,7 +61,7 @@ int calculateShortestPath(int from, int to) {
 	return costs[to];
 }
 
-void _banShortestPath(int from, int to) {
+void banShortestPath(vector<vector<int>>& traces, int from, int to) {
 	queue<int> midpoints;
 
 	midpoints.push(to);
@@ -83,7 +70,7 @@ void _banShortestPath(int from, int to) {
 		int _to = midpoints.front();
 		midpoints.pop();
 
-		for (int mid : trace[_to]) {
+		for (int mid : traces[_to]) {
 			if (banned[mid][_to] == trueVal) continue;
 
 			banned[mid][_to] = trueVal;
@@ -93,14 +80,13 @@ void _banShortestPath(int from, int to) {
 	}
 }
 
-void banShortestPath(int from, int to) {
-	_banShortestPath(from, to);
-}
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
+
+	int nodeCount;
+	int edgeCount;
 
 	int from, to;
 	int _from, _to, cost;
@@ -110,6 +96,9 @@ int main() {
 
 		if (nodeCount == 0 && edgeCount == 0) break;
 
+		vector<vector<edge>> nodes(nodeCount);
+		vector<vector<int>> traces(nodeCount);
+
 		cin >> from >> to;
 
 		for (int i = 0; i < edgeCount; i++) {
@@ -118,17 +107,17 @@ int main() {
 			nodes[_from].push_back({ _to, cost });
 		}
 
-		calculateShortestPath(from, to);
+		calculateShortestPath(nodes, traces, from, to);
 
-		banShortestPath(from, to);
+		banShortestPath(traces, from, to);
 
-		cost = calculateShortestPath(from, to);
+		cost = calculateShortestPath(nodes, traces, from, to);
 
 		cout << (cost == INT_MAX ? -1 : cost) << "\n";
 
 		for (int i = 0; i < nodeCount; i++) {
 			nodes[i].clear();
-			trace[i].clear();
+			traces[i].clear();
 		}
 
 		trueVal++;
