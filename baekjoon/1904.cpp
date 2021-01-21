124dea984ee6231e3b99bb56e83fb0ef494f6b5a
solo5star <solo_5@naver.com>
2021-01-21 08:46:27 +0000

    Test submimssion commit: id=25517774, title=01타일

---
+++ baekjoon/1904.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[1000001] = { 0, 1, 2, 3 };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	cout << dp[n];
+}
