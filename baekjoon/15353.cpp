1362557e141f9eeeedbda413c9c18ab20dd865e3
solo5star <solo_5@naver.com>
2021-01-21 11:29:29 +0000

    Test submimssion commit: id=25523613, title=큰 수 A+B (2)

--- baekjoon/15353.cpp
+++ baekjoon/15353.cpp
@@ -10,6 +10,7 @@ const data_t THRESHOLD = 17;
 const data_t THRESHOLD_MOD_BY = pow(10, THRESHOLD);
 
 typedef struct {
+	int index;
 	data_t num[1000];
 } big_int;
 
@@ -18,39 +19,48 @@ void big_int_constructor(big_int& result, const string& str) {
 	int index = 0;
 
 	for (int i = str.length() - 1; i >= 0; i--) {
-		result.num[index] += ((data_t)str[i] - '0') * multiplier;
+		result.num[result.index] += ((data_t)str[i] - '0') * multiplier;
 
 		multiplier *= 10;
 
 		if (multiplier >= THRESHOLD_MOD_BY) {
 			multiplier = 1;
-			index++;
+			result.index++;
 
-			result.num[index] = 0;
+			result.num[result.index] = 0;
 		}
 	}
+
+	if (result.num[result.index]) result.index++;
 }
 
 void big_int_add(big_int& result, const big_int& a, const big_int& b) {
 	data_t part, overflow = 0;
-	for (int i = 0; i < 1000; i++) {
+	result.index = max(a.index, b.index);
+
+	for (int i = 0; i <= result.index; i++) {
 		part = a.num[i] + b.num[i];
 
 		result.num[i] = part % THRESHOLD_MOD_BY + overflow;
 		overflow = part >= THRESHOLD_MOD_BY;
-
-		if (part == 0) break;
 	}
+
+	if (result.num[result.index]) result.index++;
 }
 
 void big_int_print(big_int& result) {
-	int i = 999;
-	while (i >= 0 && result.num[i] == 0) i--;
+	if (result.index == 0) {
+		cout << "0";
+		return;
+	}
 
-	if (i == -1) cout << "0";
-
-	while (i >= 0) {
-		cout << result.num[i--];
+	bool isHead = true;
+	for (int i = result.index - 1; i >= 0; i--) {
+		if (!isHead) {
+			cout << setfill('0') << setw(THRESHOLD);
+		}
+		cout << result.num[i];
+		isHead = false;
 	}
 }
 
