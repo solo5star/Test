9fcf86e60e1304e510354ef6e6ad3aa3e8b80a44
solo5star <solo_5@naver.com>
2021-01-21 07:06:46 +0000

    Test submimssion commit: id=25513018, title=비숍

--- baekjoon/1799.cpp
+++ baekjoon/1799.cpp
@@ -1,14 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <iostream>
-#include <utility>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <math.h>
-#include <queue>
-#include <stack>
-#include <limits.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -16,48 +8,47 @@ typedef struct {
 	int x; int y;
 } point;
 
-int map[10][10];
-bool occupation[10][10];
 int width;
 
 vector<point> points;
+vector<point> bishops;
 
-bool oddMode = true;
-
-int maxCount = 0;
-
-void dfs(int i = 0, int count = 0) {
-	if (i >= points.size()) return;
+int _dfs(bool odd, int i) {
+	if (i >= points.size()) return 0;
 
 	point& p = points[i];
 
-	if ((p.x + p.y) % 2 == (oddMode ? 1 : 0)) return;
+	if ((p.x + p.y) % 2 == odd) return 0;
 
-	point edges[4] = {
-		{ p.x - abs(p.x - p.y), p.y - abs(p.x - p.y) },
-		{ min(p.x + p.y, width - 1), (p.x + p.y) % (width - 1) }
-	};
-	edges[2] = { edges[0].y, edges[0].x };
-	edges[3] = { edges[1].y, edges[1].x };
-
-	for (int i = 0; i < 4; i++) {
-		if (occupation[edges[i].y][edges[i].x]) return;
+	for (point& b : bishops) {
+		if (abs(p.x - b.x) == abs(p.y - b.y)) return 0;
 	}
 
-	for (int i = 0; i < 4; i++) {
-		occupation[edges[i].y][edges[i].x] = true;
-	}
-
-	count++;
-	maxCount = max(maxCount, count);
+	bishops.push_back(p);
+	
+	int maxCount = bishops.size();
 
 	for (i++; i < points.size(); i++) {
-		dfs(i, count);
+		maxCount = max(maxCount, _dfs(odd, i));
 	}
 
-	for (int i = 0; i < 4; i++) {
-		occupation[edges[i].y][edges[i].x] = false;
-	}
+	bishops.pop_back();
+
+	return maxCount;
+}
+
+int dfs() {
+	int sum = 0;
+
+	int maxCount = 0;
+	for (int i = 0; i < points.size(); i++) maxCount = max(maxCount, _dfs(true, i));
+	sum += maxCount;
+
+	maxCount = 0;
+	for (int i = 0; i < points.size(); i++) maxCount = max(maxCount, _dfs(false, i));
+	sum += maxCount;
+
+	return sum;
 }
 
 int main() {
@@ -66,25 +57,15 @@ int main() {
 	cout.tie(nullptr);
 
 	cin >> width;
+
+	int canPlaceBishop;
 	for (int y = 0; y < width; y++) {
 		for (int x = 0; x < width; x++) {
-			cin >> map[y][x];
-			//map[y][x] = 1;
+			cin >> canPlaceBishop;
 
-			if (map[y][x] == 1) points.push_back({ x, y });
+			if (canPlaceBishop) points.push_back({ x, y });
 		}
 	}
 
-	int sum = 0;
-
-	oddMode = true;
-	for(int i = 0; i < points.size(); i++) dfs(i);
-	sum += maxCount;
-
-	maxCount = 0;
-	oddMode = false;
-	for (int i = 0; i < points.size(); i++) dfs(i);
-	sum += maxCount;
-
-	cout << sum;
+	cout << dfs();
 }
