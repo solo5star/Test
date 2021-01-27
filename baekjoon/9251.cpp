4a44f0727af97568429032a53775217ffacfd7ba
solo5star <solo_5@naver.com>
2021-01-27 07:32:56 +0000

    Test submimssion commit: id=25734295, title=LCS

--- baekjoon/9251.cpp
+++ baekjoon/9251.cpp
@@ -1,40 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <iostream>
-#include <utility>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <math.h>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-// 1d: str1
-// 2d: str2
+string str1, str2, str3;
+
 int dp[1001][1001];
 
 int main() {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	string str1, str2;
-	cin >> str2 >> str1;
+	cin >> str1 >> str2;
 
-	int maxValue = 0;
+	for (int i = 1; i <= str1.length(); i++) {
+		for (int j = 1; j <= str2.length(); j++) {
+			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 
-	for (int i = 0; i < str1.length(); i++) {
-		for (int j = 0; j < str2.length(); j++) {
-			if (str1[i] == str2[j]) {
-				dp[i + 1][j + 1] = dp[i][j] + 1;
+			if (str1[i - 1] == str2[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
 			}
-			else {
-				dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
-			}
-			maxValue = max(maxValue, dp[i + 1][j + 1]);
 		}
 	}
 
-	cout << maxValue;
+	cout << dp[str1.length()][str2.length()];
 }
